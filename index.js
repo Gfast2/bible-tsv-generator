7b8089a59baaba936c4a44ae43b8048a287d7934
gfast2 <gfast2@hotmail.com>
2019-11-26 19:07:38 +0100

    Designed a recrusive function for getting books one by one.

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 const englishNamePairs = require('./getEnglishNamePairs');
 const fetchBook = require('./fetchBook');
 
+const bibleObj = {}; // Whole bible in Json format
+
+const recrusiveBookFetcher = async bookNames => {
+  const namePairs = Object.entries(bookNames);
+  const [book2Read, ...bookRest] = namePairs;
+  const bookFullName = book2Read[0];
+  const bookShortName = book2Read[1];
+  const restBooks = {};
+  namePairs.map(([fullName, shortName]) => {
+    if (fullName !== bookFullName) {
+      restBooks[fullName] = shortName;
+    }
+  });
+  try {
+    const bookObj = await fetchBook(bookShortName);
+    bibleObj[bookFullName] = bookObj;
+    if (bookRest.length === 0) {
+      return Promise.resolve('All books get fetched sucessfully');
+    }
+    return await recrusiveBookFetcher(restBooks);
+  } catch (e) {
+    return Promise.reject(`Error triggered while fetching book ${bookFullName}`);
+  }
+};
+
 englishNamePairs().then(
   resolved => {
     // TODO: fetch data from internet books for books
-    const tmp = {
-      Genesis: 'Gen',
-    };
-    fetchBook(tmp.Genesis).then(
-      resolve => {
-        console.log(resolve);
+    recrusiveBookFetcher(resolved).then(
+      resol => {
+        console.log(resol);
       },
-      reje => {
-        console.log('rejected while fetching data.');
-        console.log(reje);
+      rejec => {
+        console.log('rejec');
+        console.log(rejec);
       }
     );
   },
   reason => {
-    console.log('rejected while parsing english full / short name paars.');
+    console.log('rejected while parsing english full / short name pairs.');
     console.log(reason);
   }
 );
