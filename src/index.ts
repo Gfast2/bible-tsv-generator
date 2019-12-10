ff59dfb4ae03567803db97759a64d57a651812f7
gfast2 <gfast2@hotmail.com>
2019-12-10 19:13:59 +0100

    Setup tsconfig for typscript & convert index.js to ts with any

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,20 @@ const fetchBooks = require('./fetchBooks');
 const write2fs = require('./write2fs');
 const chalk = require('chalk');
 
-const writeTsv = async (obj, _bookNamePairs) => {
-  for await (const [bookId, objBody] of Object.entries(obj)) {
+interface Verse {
+  id: string;
+  content: string;
+}
+
+const writeTsv = async (obj:any, _bookNamePairs:any) => {
+  for await (const [bookId, objBody] of Object.entries<object>(obj)) {
     const nameObj = _bookNamePairs[bookId];
     const zhFullName = nameObj['zh_hans_full'];
     const zhShortName = nameObj['zh_hans_short'];
     console.log(chalk.bold.dim('Writing down book ') + chalk.inverse.whiteBright(zhFullName));
     for await (const [key, value] of Object.entries(objBody)) {
       if (key === 'book') {
-        for await (const [k, verses] of Object.entries(value)) {
+        for await (const [k, verses] of Object.entries<object>(value)) {
           const chapterID = k;
           for await (const [verseID, verseContent] of Object.entries(verses)) {
             const line = `${zhFullName}\t${zhShortName}\t${bookId}\t${chapterID}\t${verseID}\t${verseContent}\n`;
@@ -62,10 +67,10 @@ const doneGraph = () =>
   );
 
 zhHans().then(
-  resolved => {
+  (resolved:any) => {
     console.log('Got all simplified chinese book titles.');
     fetchBooks().then(
-      async resol => {
+      async (resol:any) => {
         console.log('Got all simplified chinese book contents.');
         const wholeBible = resol.version;
         const bookNamePairs = resolved;
@@ -86,13 +91,13 @@ zhHans().then(
           }
         );
       },
-      rejec => {
+      (rejec:any) => {
         console.log(chalk.bold.underline.redBright('Failed to fetch & parse whole bible with following reason:'));
         console.log(rejec);
       }
     );
   },
-  reason => {
+  (reason:any) => {
     console.log(chalk.bold.underline.redBright('Got rejected while fetching book titles.'));
     console.log(reason);
   }
